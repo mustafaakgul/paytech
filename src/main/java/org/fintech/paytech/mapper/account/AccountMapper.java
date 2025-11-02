e8de262ad070e2469d629993ced9697d0452c917
Mustafa Akgül <mustafaakguldev@icloud.com>
2025-11-02 15:06:15 +0300

    Add mappers

---
+++ src/main/java/org/fintech/paytech/mapper/account/AccountMapper.java
@@ -0,0 +1,16 @@
+package org.fintech.paytech.mapper.account;
+
+/*import org.fintech.budget.domains.account.models.Account;
+import org.fintech.budget.domain.core.account.dto.AccountDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AccountMapper {
+
+    AccountMapper INSTANCE = Mappers.getMapper(AccountMapper.class);
+
+    @Mapping(target = "id", ignore = true) // Ingore ID for DTO conversion
+    AccountDTO entityToDto(Account customer);
+}*/
