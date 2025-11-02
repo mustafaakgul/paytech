3e8350d4da2350ff7ca21b5aa30a7d4051f97c0b
Mustafa Akgül <mustafaakguldev@icloud.com>
2025-11-02 12:52:36 +0300

    Add portfolio module

---
+++ src/main/java/org/fintech/paytech/domain/core/portfolio/model/Portfolio.java
@@ -0,0 +1,26 @@
+package org.fintech.paytech.domain.core.portfolio.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import org.fintech.paytech.domain.core.asset.model.Asset;
+import org.fintech.paytech.domain.core.user.model.User;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "asset_id")
+    private Asset asset;
+}
